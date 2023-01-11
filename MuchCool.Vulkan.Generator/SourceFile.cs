f8b419fccad5a586e2d8250516c137504bfe365a
Jacob Green <jacobrgreen114@gmail.com>
2023-01-11 18:38:03 -0500

    Update copyright headers

--- MuchCool.Vulkan.Generator/SourceFile.cs
+++ MuchCool.Vulkan.Generator/SourceFile.cs
@@ -1,4 +1,20 @@
-﻿namespace MuchCool.Vulkan.Generator; 
+﻿/* Copyright (c) 2023 Jacob R. Green
+ *
+ * This file is part of MuchCool.Vulkan.
+ *
+ * MuchCool.Vulkan is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MuchCool.Vulkan is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MuchCool.Vulkan. If not,
+ * see <https://www.gnu.org/licenses/>.
+ */
+
+namespace MuchCool.Vulkan.Generator; 
 
 public class SourceFile {
     private readonly SourceBuilder _builder = new();
