fd96de044ffcbc7acb32c6c178b54991e1681c42
Jacob Green <jacobrgreen114@gmail.com>
2023-01-11 23:57:58 -0500

    update copyright header

--- MuchCool.Vulkan/Native/SafeWrappers.cs
+++ MuchCool.Vulkan/Native/SafeWrappers.cs
@@ -1,10 +1,23 @@
-﻿using System.Runtime.InteropServices;
+﻿/* Copyright (c) 2023 Jacob R. Green
+ *
+ * This file is part of MuchCool.Vulkan.
+ *
+ * MuchCool.Vulkan is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MuchCool.Vulkan is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MuchCool.Vulkan. If not,
+ * see <https://www.gnu.org/licenses/>.
+ */
+
 
 namespace MuchCool.Vulkan.Native; 
 
-public static class Vk {
-
-
+public static partial class Vk {
     #region Instance
 
     public static unsafe VkInstance CreateInstance(PFN_vkCreateInstance proc, in VkInstanceCreateInfo createInfo) {
